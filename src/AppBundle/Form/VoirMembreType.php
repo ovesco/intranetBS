b4015e0cd0bfbebf400c8ab1db9a4828501584bf
Christian Muller <christian.muller@sauvabelin.ch>
2015-02-13 17:51:34 +0100

    - Modif page groupe

--- src/AppBundle/Form/VoirMembreType.php
+++ src/AppBundle/Form/VoirMembreType.php
@@ -7,9 +7,7 @@ use AppBundle\Field\MembreFamilleType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormInterface;
+
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 use AppBundle\Field\SemanticType;
