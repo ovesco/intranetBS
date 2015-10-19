d051ed0968a9412d1e565a2dc0c1f41be2df1481
Christian Muller <christian.muller@sauvabelin.ch>
2015-10-19 22:41:48 +0200

    Listes de données - Implémentation des nouvelles listes sur la pages des membres - Ajout de liens sur les listes - Fix le bug des parameters (un peu moche j'avoue)

--- src/AppBundle/Utils/ListRenderer/ListModels/ListModelsAttributions.php
+++ src/AppBundle/Utils/ListRenderer/ListModels/ListModelsAttributions.php
@@ -7,33 +7,35 @@ use AppBundle\Entity\Attribution;
 use AppBundle\Utils\ListRenderer\ActionLigne;
 use AppBundle\Utils\ListRenderer\Column;
 use AppBundle\Utils\ListRenderer\ListRenderer;
+use Symfony\Component\Routing\Router;
 
 class ListModelsAttributions
 {
 
     /**
      * @param \Twig_Environment $twig
+     * @param Router $router
      * @param $items
      * @return ListRenderer
      */
-    static public function getDefault(\Twig_Environment $twig, $items)
+    static public function getDefault(\Twig_Environment $twig, Router $router, $items)
     {
         $list = new ListRenderer($twig, $items);
 
         $list->setSearchBar(true);
 
-        $list->addColumn(new Column('Fonction', function (Attribution $item) {
-            return $item->getFonction();
+        $list->addColumn(new Column('Fonction', function (Attribution $attribution) {
+            return $attribution->getFonction();
         }));
-        $list->addColumn(new Column('Unité', function (Attribution $item) {
-            return $item->getGroupe();
+        $list->addColumn(new Column('Unité', function (Attribution $attribution) use ($router) {
+            return '<a href="' . $router->generate('interne_voir_groupe', array('groupe' => $attribution->getGroupe()->getId())) . '">' . $attribution->getGroupe() . '</a>';
         }));
-        $list->addColumn(new Column('Depuis le', function (Attribution $item) {
-            return $item->getDateDebut();
+        $list->addColumn(new Column('Depuis le', function (Attribution $attribution) {
+            return $attribution->getDateDebut();
         },
             'date(global_date_format)'));
-        $list->addColumn(new Column('Jusqu\'au', function (Attribution $item) {
-            return $item->GetDateFin();
+        $list->addColumn(new Column('Jusqu\'au', function (Attribution $attribution) {
+            return $attribution->GetDateFin();
         },
             'date(global_date_format)'));
 
--- src/AppBundle/Utils/ListRenderer/ListModels/ListModelsDistinctions.php
+++ src/AppBundle/Utils/ListRenderer/ListModels/ListModelsDistinctions.php
@@ -3,29 +3,31 @@
 
 namespace AppBundle\Utils\ListRenderer\ListModels;
 
-use AppBundle\Entity\Distinction;
+use AppBundle\Entity\ObtentionDistinction;
 use AppBundle\Utils\ListRenderer\ActionLigne;
 use AppBundle\Utils\ListRenderer\Column;
 use AppBundle\Utils\ListRenderer\ListRenderer;
+use Symfony\Component\Routing\Router;
 
 class ListModelsDistinctions
 {
 
     /**
      * @param \Twig_Environment $twig
+     * @param Router $router
      * @param $items
      * @return ListRenderer
      */
-    static public function getDefault(\Twig_Environment $twig, $items)
+    static public function getDefault(\Twig_Environment $twig, Router $router, $items)
     {
         $list = new ListRenderer($twig, $items);
 
         $list->setSearchBar(true);
 
-        $list->addColumn(new Column('Distinction', function (Distinction $item) {
-            return $item->getNom();
+        $list->addColumn(new Column('Distinction', function (ObtentionDistinction $item) {
+            return $item->getDistinction()->getNom();
         }));
-        $list->addColumn(new Column('Depuis le', function (Distinction $item) {
+        $list->addColumn(new Column('Depuis le', function (ObtentionDistinction $item) {
             return $item->getDate();
         },
             'date(global_date_format)'));
