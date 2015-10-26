e55b125e0f50603ec577b7d26351ba75b29b4b67
nicolas uffer <nicolas.uffer@gmail.com>
2015-10-26 16:17:14 +0100

    petite modif du menu

---
+++ src/AppBundle/Resources/public/js/alwaysIncluded/popup-menu.js
@@ -0,0 +1,20 @@
+/**
+ * Created by NicolasUffer on 12.10.15.
+ */
+
+$(document).ready(function(){
+
+
+    $('.popup-menu').popup({
+        hoverable: true,
+        position : 'right center',
+        exclusive: true,
+        delay: {
+            show: 10,
+            hide: 500
+        }
+    });
+
+});
+
+
