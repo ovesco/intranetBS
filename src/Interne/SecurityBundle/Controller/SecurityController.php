9cea0f725286aa01e1700be085c07fbfea338511
Christian Muller <christian.muller@sauvabelin.ch>
2015-10-28 22:45:26 +0100

    - Fix import doublons

--- src/Interne/SecurityBundle/Controller/SecurityController.php
+++ src/Interne/SecurityBundle/Controller/SecurityController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class SecurityController extends Controller
 {
