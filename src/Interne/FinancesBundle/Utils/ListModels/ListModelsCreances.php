24eae3face1f8f0e7202f5f566941c92786975e6
Christian Muller <christian.muller@sauvabelin.ch>
2015-11-04 22:32:33 +0100

    - Liste creance et factures

--- src/Interne/FinancesBundle/Utils/ListModels/ListModelsCreances.php
+++ src/Interne/FinancesBundle/Utils/ListModels/ListModelsCreances.php
@@ -3,26 +3,28 @@
 
 namespace Interne\FinancesBundle\Utils\ListModels;
 
+use AppBundle\Utils\Event\EventPostAction;
 use AppBundle\Utils\ListRenderer\Action;
 use AppBundle\Utils\ListRenderer\Column;
 use AppBundle\Utils\ListRenderer\ListRenderer;
 use Interne\FinancesBundle\Entity\Creance;
+use Symfony\Component\Routing\Router;
 
 class ListModelsCreances
 {
 
     /**
      * @param \Twig_Environment $twig
+     * @param Router $router
      * @param $items
      * @return ListRenderer
      */
-    static public function getDefault(\Twig_Environment $twig, $items)
+    static public function getDefault(\Twig_Environment $twig, Router $router, $items)
     {
         $list = new ListRenderer($twig, $items);
 
         $list->setSearchBar(true);
 
-        /*
 
         $list->addColumn(new Column('Facture', function (Creance $item) {
             if ($item->getFacture() != null)
@@ -30,25 +32,29 @@ class ListModelsCreances
             else
                 return '<i class="bordered inverted orange wait icon popupable" data-content="En attente"></i>';
         }));
-        */
 
-        $list->addColumn(new Column('Motif', function (Creance $item) {
-            return $item->getTitre();
+        $list->addColumn(new Column('Motif', function (Creance $creance) use ($router) {
+            return '<a href="' . $router->generate('interne_finances_creance_show', array('creance' => $creance->getId())) . '">' . $creance->getTitre() . '</a>';
         }));
 
-        /*
-        $list->addColumn(new Column('Montant', function (Creance $item) {
-            return $item->getMontantEmis();
-        }, "number_format(2, '.', ',')"));
-        $list->addColumn(new Column('Montant perçu', function (Creance $item) {
-            return $item->getMontantRecu();
+        $list->addColumn(new Column('Montant', function (Creance $creance) {
+            return $creance->getMontantEmis();
         }, "number_format(2, '.', ',')"));
 
-        $list->addAction(new Action('Afficher', 'zoom icon popupable', 'event_creance_show'));
-        $list->addAction(new Action('Supprimer', 'delete icon popupable', 'event_creance_delete'));
+        $list->addColumn(new Column('Montant perçu', function (Creance $creance) {
+            return $creance->getMontantRecu();
+        }, "number_format(2, '.', ',')"));
 
-        */
-        //$list->setDatatable(true);
+
+        $creanceParameters = function (Creance $creance) {
+            return array(
+                'creance' => $creance->getId()
+            );
+        };
+
+        $list->addAction(new Action('Supprimer', 'delete', 'interne_finances_creance_delete', $creanceParameters, EventPostAction::RefreshList));
+
+        $list->setDatatable(true);
 
         return $list;
     }
--- src/Interne/FinancesBundle/Utils/ListModels/ListModelsFactures.php
+++ src/Interne/FinancesBundle/Utils/ListModels/ListModelsFactures.php
@@ -3,30 +3,33 @@
 
 namespace Interne\FinancesBundle\Utils\ListModels;
 
+use AppBundle\Utils\Event\EventPostAction;
 use AppBundle\Utils\ListRenderer\Action;
 use AppBundle\Utils\ListRenderer\Column;
 use AppBundle\Utils\ListRenderer\ListRenderer;
 use Interne\FinancesBundle\Entity\Facture;
+use Symfony\Component\Routing\Router;
 
 class ListModelsFactures
 {
 
     /**
      * @param \Twig_Environment $twig
+     * @param Router $router
      * @param $items
      * @return ListRenderer
      */
-    static public function getDefault(\Twig_Environment $twig, $items)
+    static public function getDefault(\Twig_Environment $twig, Router $router, $items)
     {
         $list = new ListRenderer($twig, $items);
 
         $list->setSearchBar(true);
 
-        $list->addColumn(new Column('Num. ref', function (Facture $item) {
-            return 'N°' . $item->GetId();
+        $list->addColumn(new Column('Num. ref', function (Facture $facture) use ($router) {
+            return '<a href="' . $router->generate('interne_finances_facture_show', array('facture' => $facture->getId())) . '">N°' . $facture->getId() . '</a>';
         }));
-        $list->addColumn(new Column('Statut', function (Facture $item) {
-            switch ($item->getStatut()) {
+        $list->addColumn(new Column('Statut', function (Facture $facture) {
+            switch ($facture->getStatut()) {
                 case 'payee':
                     return '<i class="bordered inverted green checkmark icon popupable" data-content="Payée"></i>';
                     break;
@@ -36,23 +39,29 @@ class ListModelsFactures
                     break;
             }
         }));
-        $list->addColumn(new Column('Créances', function (Facture $item) {
-            return $item->getMontantEmisCreances();
+        $list->addColumn(new Column('Créances', function (Facture $facture) {
+            return $facture->getMontantEmisCreances();
         }, "number_format(2, '.', ',')"));
-        $list->addColumn(new Column('Rappels', function (Facture $item) {
-            return $item->getMontantEmisRappels();
+        $list->addColumn(new Column('Rappels', function (Facture $facture) {
+            return $facture->getMontantEmisRappels();
         }, "number_format(2, '.', ',')"));
-        $list->addColumn(new Column('Total', function (Facture $item) {
-            return $item->getMontantEmis();
+        $list->addColumn(new Column('Total', function (Facture $facture) {
+            return $facture->getMontantEmis();
         }, "number_format(2, '.', ',')"));
-        $list->addColumn(new Column('Reçu', function (Facture $item) {
-            return $item->getMontantRecu();
+        $list->addColumn(new Column('Reçu', function (Facture $facture) {
+            return $facture->getMontantRecu();
         }, "number_format(2, '.', ',')"));
 
-        $list->addAction(new Action('Afficher', 'zoom icon popupable', 'event_creance_show'));
-        $list->addAction(new Action('Supprimer', 'delete icon popupable', 'event_creance_delete'));
 
-        $list->setDatatable(false);
+        $factureParameters = function (Facture $facture) {
+            return array(
+                'facture' => $facture->getId()
+            );
+        };
+
+        $list->addAction(new Action('Supprimer', 'delete', 'interne_finances_facture_delete', $factureParameters, EventPostAction::RefreshList));
+
+        $list->setDatatable(true);
 
         return $list;
     }
