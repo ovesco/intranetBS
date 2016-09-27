07677ba48bbdf7163185053a19b22a687096f895
Christian Muller <christian.muller@sauvabelin.ch>
2016-09-27 22:27:52 +0200

    - Template des forms par défaut

--- src/AppBundle/Resources/public/js/Layout/modal_display.js
+++ src/AppBundle/Resources/public/js/Layout/modal_display.js
@@ -8,23 +8,23 @@ function getModal(data, url) {
         type: "POST",
         url: url,
         data: data,
-        error: function(xhr, ajaxOptions, thrownError) {
+        error: function (xhr, ajaxOptions, thrownError) {
             alerte.send("Erreur lors de l'ouverture de la fenêtre.<br />Détails : " + xhr.status + " / " + thrownError, 'error');
         },
-        success: function(htmlResponse) {
+        success: function (htmlResponse) {
             $(htmlResponse).modal('show');
         }
     });
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     /**
      * Appel une modal (cf. Twig:AppExtension:modal_caller)
      */
-    $(document).on('click','.modal_caller',function(){
+    $(document).on('click', '.modal_caller', function () {
         var url = $(this).data('modal-url');
-        getModal(null,url);
+        getModal(null, url);
 
     });
 
